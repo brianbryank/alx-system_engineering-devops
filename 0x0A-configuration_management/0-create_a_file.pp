9a1ae5d82a75e499469b62972aaa45ee0dde469d
brianbryank <brianbryan0125@gmail.com>
2022-05-11 09:58:38 +0300

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 #  create a file in /tmp using puppet
 file { '/tmp/school':
-    ensure  => 'present',
+    ensure  => 'absent',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
