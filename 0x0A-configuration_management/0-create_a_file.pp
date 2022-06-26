5e61370ae8d7255530c8007cb7a81636d72c321b
brianbryank <brianbryan0125@gmail.com>
2022-06-26 10:37:49 +0300

    upate the puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
-#  create a file in /tmp using puppet
-file { '/tmp/school':
-    ensure  => 'absent',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet', }
+# Create a file in /tmp
+
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
