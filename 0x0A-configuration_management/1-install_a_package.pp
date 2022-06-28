1eaee54d475b8d09664d0eb8a6ffd0f18f821d00
brianbryank <brianbryan0125@gmail.com>
2022-06-28 17:12:32 +0300

    update thr

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# This Puppet manifest installs puppet-lint
-
+# Installs puppet-lint using Puppet
 package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+    ensure   => '2.5.0',
+    provider => 'gem',
 }
