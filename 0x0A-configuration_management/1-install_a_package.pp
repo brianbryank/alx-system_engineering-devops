d7b9300edcfd53ec3f323cc9d206f81ea2bbe37d
brianbryank <brianbryan0125@gmail.com>
2022-06-28 09:50:07 +0300

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# install puppet
+# Installs puppet-lint using Puppet
 package { 'puppet-lint':
-    ensure   => '2.1.0',
-name => 'puppet-lint',
+    ensure   => '2.5.0',
     provider => 'gem',
 }
